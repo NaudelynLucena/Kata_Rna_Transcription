4d3713bc8ad63dea9f1ae0a2fe69ad80f41f2e15
Naudelyn Lucena <you@example.com>
2024-11-24 22:53:44 +0100

    Ejercicio realizado. Test modificado, cobertura 100%

--- src/main/java/org/factoriaf5/rna_transcription/RnaTranscription.java
+++ src/main/java/org/factoriaf5/rna_transcription/RnaTranscription.java
@@ -1,10 +1,29 @@
 package org.factoriaf5.rna_transcription;
 
-
 public class RnaTranscription {
-    
-    String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Implement method");
-    }
 
+    public static String toRNA(String dna) {
+        StringBuilder rna = new StringBuilder();
+
+        for (char nucleotide : dna.toCharArray()) {
+            switch (nucleotide) {
+                case 'G':
+                    rna.append('C');
+                    break;
+                case 'C':
+                    rna.append('G');
+                    break;
+                case 'T':
+                    rna.append('A');
+                    break;
+                case 'A':
+                    rna.append('U');
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid nucleotide in DNA sequence: " + nucleotide);
+            }
+        }
+
+        return rna.toString();
+    }
 }
--- src/test/java/org/factoriaf5/rna_transcription/RnaTranscriptionTest.java
+++ src/test/java/org/factoriaf5/rna_transcription/RnaTranscriptionTest.java
@@ -5,50 +5,42 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class RnaTranscriptionTest {
 
-    private RnaTranscription rnaTranscription;
-
     @BeforeEach
     public void setUp() {
-        rnaTranscription = new RnaTranscription();
+        new RnaTranscription();
     }
 
     @Test
     public void testEmptyRnaSequence() {
-        assertThat(rnaTranscription.transcribe("").isEmpty(), is(true));
+        assertThat(RnaTranscription.toRNA("").isEmpty(), is(true));
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testRnaTranscriptionOfCytosineIsGuanine() {
-        assertThat(rnaTranscription.transcribe("C"), is(equalTo("G")));
+        assertThat(RnaTranscription.toRNA("C"), is(equalTo("G")));
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testRnaTranscriptionOfGuanineIsCytosine() {
-        assertThat(rnaTranscription.transcribe("G"), is(equalTo("C")));
+        assertThat(RnaTranscription.toRNA("G"), is(equalTo("C")));
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testRnaTranscriptionOfThymineIsAdenine() {
-        assertThat(rnaTranscription.transcribe("T"), is(equalTo("A")));
+        assertThat(RnaTranscription.toRNA("T"), is(equalTo("A")));
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testRnaTranscriptionOfAdenineIsUracil() {
-        assertThat(rnaTranscription.transcribe("A"), is(equalTo("U")));
+        assertThat(RnaTranscription.toRNA("A"), is(equalTo("U")));
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testRnaTranscription() {
-        assertThat(rnaTranscription.transcribe("ACGTGGTCTTAA"), is(equalTo("UGCACCAGAAUU")));
+        assertThat(RnaTranscription.toRNA("ACGTGGTCTTAA"), is(equalTo("UGCACCAGAAUU")));
     }
 }
